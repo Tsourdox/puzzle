c14d1d7dc7d877922d008312f13c97c3d6088b00
David Jensen <tsourdox@icloud.com>
2023-09-13 09:48:18 +0200

    remove colortheif and use bg image instead

--- app/room/[code]/canvas.tsx
+++ app/room/[code]/canvas.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import { useRef } from 'react';
+import { globals } from './utils/globals';
 import usePuzzle from './utils/usePuzzle';
 
 export default function PuzzleCanvas() {
   const containerRef = useRef<HTMLDivElement>(null);
   usePuzzle(containerRef);
 
-  return <div ref={containerRef} className="relative flex-1" />;
+  return (
+    <div
+      className="flex flex-col flex-1 bg-cover"
+      style={{ backgroundImage: `url('${globals.imageSrc}')` }}
+    >
+      <div
+        ref={containerRef}
+        className="relative flex-1 backdrop-blur-3xl bg-neutral-800/70"
+      />
+    </div>
+  );
 }
