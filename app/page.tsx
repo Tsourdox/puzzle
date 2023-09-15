e4f4e0d07b3fe23771648de843229d60deede99e
David Jensen <tsourdox@icloud.com>
2023-09-15 16:41:48 +0200

    add beta label (prepare for release)

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,13 @@ export default function HomePage() {
   return (
     <div className="flex flex-col py-16 gap-16 text-neutral-100 bg-gradient-to-bl from-[#210034] via-20% via-neutral-950 to-100% to-[#110024]">
       <header className="flex flex-col gap-4 items-center">
-        <h1 className="text-6xl text-transparent bg-clip-text bg-gradient-to-l from-purple-700 to-purple-100">
-          PUZZELIN
+        <h1 className="relative">
+          <span className="text-6xl text-transparent uppercase bg-clip-text bg-gradient-to-l from-purple-700 to-purple-100">
+            Puzzelin
+          </span>
+          <span className="uppercase text-2xl text-purple-300/70 absolute -top-5 right-1">
+            BETA
+          </span>
         </h1>
         <span className="text-2xl font-thin text-neutral-300 border-t-1 border-neutral-800 px-4 font-sans">
           Ett online pussel för dig och dina vänner!
