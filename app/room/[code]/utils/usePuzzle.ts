c31ae7b03428e83d4d87933ddbac35daa274dab5
David Jensen <tsourdox@icloud.com>
2023-09-15 16:13:00 +0200

    use dynamic imports

--- app/room/[code]/utils/usePuzzle.ts
+++ app/room/[code]/utils/usePuzzle.ts
@@ -7,91 +7,95 @@ export default function usePuzzle(
   onReady: () => void,
 ) {
   useEffect(() => {
-    const p5 = require('p5');
-    const { default: Puzzle } = require('@/puzzle/puzzle');
-
-    if (!containerRef.current) throw Error('Could not mount canvas');
-    const { width, height } = containerRef.current.getBoundingClientRect();
-
     let puzzle: Puzzle;
 
-    const sketch = (p: p5) => {
-      function getPiecesCountFromSize(size: string) {
-        switch (size) {
-          case 'XS':
-            return 4;
-          case 'S':
-            return 8;
-          case 'M':
-            return 12;
-          case 'L':
-            return 20;
-          case 'XL':
-          default:
-            return 30;
+    (async () => {
+      // NextJS SSR crashes when importing p5 - dynamic import solves it.
+      const { default: p5 } = await import('p5');
+      const { default: Puzzle } = await import('@/puzzle/puzzle');
+
+      if (!containerRef.current) throw Error('Could not mount canvas');
+      const { width, height } = containerRef.current.getBoundingClientRect();
+
+      const sketch = (p: p5) => {
+        function getPiecesCountFromSize(size: string) {
+          switch (size) {
+            case 'XS':
+              return 4;
+            case 'S':
+              return 8;
+            case 'M':
+              return 12;
+            case 'L':
+              return 20;
+            case 'XL':
+            default:
+              return 30;
+          }
         }
-      }
 
-      function getThemeFromCSS() {
-        const rootStyle = getComputedStyle(document.documentElement);
-        globals.theme = {
-          primary: rootStyle.getPropertyValue('--primary'),
-          darkened: rootStyle.getPropertyValue('--darkened'),
-          neutral: rootStyle.getPropertyValue('--neutral'),
-          background: rootStyle.getPropertyValue('--background'),
-          backdrop: rootStyle.getPropertyValue('--backdrop'),
-          darkdrop: rootStyle.getPropertyValue('--darkdrop'),
+        function getThemeFromCSS() {
+          const rootStyle = getComputedStyle(document.documentElement);
+          globals.theme = {
+            primary: rootStyle.getPropertyValue('--primary'),
+            darkened: rootStyle.getPropertyValue('--darkened'),
+            neutral: rootStyle.getPropertyValue('--neutral'),
+            background: rootStyle.getPropertyValue('--background'),
+            backdrop: rootStyle.getPropertyValue('--backdrop'),
+            darkdrop: rootStyle.getPropertyValue('--darkdrop'),
+          };
+        }
+
+        function preventDefaultEvents() {
+          // Prevent context menu when right clicking
+          document.oncontextmenu = () => false;
+          // Prenent magnifying glass
+          document
+            .querySelector('canvas')
+            ?.addEventListener('touchstart', (e) => e.preventDefault());
+        }
+
+        // The sketch setup method
+        p.setup = () => {
+          p.createCanvas(width, height);
+          p.frameRate(120);
+          preventDefaultEvents();
+          getThemeFromCSS();
+          // setSoundVolumes();
+          globals.isMobile = p.windowWidth < 600;
+
+          puzzle = new Puzzle(p);
+          const xy = getPiecesCountFromSize(globals.size);
+          puzzle.generateNewPuzzle(globals.imageSrc, xy, xy).then(() => {
+            onReady();
+            p.loop();
+          });
+          p.noLoop();
         };
-      }
 
-      function preventDefaultEvents() {
-        // Prevent context menu when right clicking
-        document.oncontextmenu = () => false;
-        // Prenent magnifying glass
-        document
-          .querySelector('canvas')
-          ?.addEventListener('touchstart', (e) => e.preventDefault());
-      }
+        // The sketch draw method
+        p.draw = () => {
+          puzzle.update();
+          puzzle.draw();
+          globals.scrollDelta = 0;
+        };
 
-      // The sketch setup method
-      p.setup = () => {
-        p.createCanvas(width, height);
-        p.frameRate(120);
-        preventDefaultEvents();
-        getThemeFromCSS();
-        // setSoundVolumes();
-        globals.isMobile = p.windowWidth < 600;
+        p.windowResized = () => {
+          const { width, height } =
+            containerRef.current!.getBoundingClientRect();
+          globals.isMobile = p.windowWidth < 600;
+          p.resizeCanvas(width, height);
+        };
 
-        puzzle = new Puzzle(p);
-        const xy = getPiecesCountFromSize(globals.size);
-        puzzle.generateNewPuzzle(globals.imageSrc, xy, xy).then(() => {
-          onReady();
-          p.loop();
-        });
-        p.noLoop();
+        p.mouseWheel = (event: any) => {
+          globals.scrollDelta = event.delta;
+          return false;
+        };
       };
 
-      // The sketch draw method
-      p.draw = () => {
-        puzzle.update();
-        puzzle.draw();
-        globals.scrollDelta = 0;
-      };
-
-      p.windowResized = () => {
-        const { width, height } = containerRef.current!.getBoundingClientRect();
-        globals.isMobile = p.windowWidth < 600;
-        p.resizeCanvas(width, height);
-      };
-
-      p.mouseWheel = (event: any) => {
-        globals.scrollDelta = event.delta;
-        return false;
-      };
-    };
-
-    new p5(sketch, containerRef.current);
-    document.body.style.overflow = 'hidden';
+      new p5(sketch, containerRef.current);
+      document.body.style.overflow = 'hidden';
+    })();
     return () => {
       document.body.style.overflow = 'unset';
       puzzle.releaseCanvas();
