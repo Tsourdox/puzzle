e821eea293bc20bc75fa3dea4d4cbe9becb28c11
David Jensen <tsourdox@icloud.com>
2023-10-14 23:38:39 +0200

    fix image preview resize visual bug

--- app/[lang]/room/[...slug]/components/ImagePreview.tsx
+++ app/[lang]/room/[...slug]/components/ImagePreview.tsx
@@ -12,41 +12,48 @@ type Props = {
 export default function ImagePreview({ image }: Props) {
   const [fullscreen, setFullscreen] = useState(false);
 
+  const aspectRatio = image.width / image.height;
+  const width = fullscreen ? 'calc(100% - 2rem)' : `calc(((10vw + 10vh) / 2) * ${aspectRatio})`;
+  const height = fullscreen ? 'calc(100% - 2rem)' : 'calc((10vw + 10vh) / 2)';
+
   return (
-    <aside
-      onClick={() => setFullscreen(invert)}
-      className={twMerge(
-        'fixed left-0 bottom-0 p-6 w-44 h-32 sm:w-60 sm:h-40 z-40 transition-all duration-500 cursor-pointer',
-        fullscreen && 'w-full h-full sm:h-full sm:w-full backdrop-blur-lg',
-      )}
-    >
+    <>
       <div
+        onClick={() => setFullscreen(invert)}
         className={twMerge(
-          'relative w-full h-full rounded-lg overflow-hidden',
-          !fullscreen && 'backdrop-blur-lg bg-neutral-950/20',
+          'fixed left-0 top-0 z-40 transition-all duration-500 cursor-pointer w-full h-full',
+          fullscreen && 'backdrop-blur-lg bg-neutral-950/20',
+          !fullscreen && 'pointer-events-none',
         )}
+      />
+      <div
+        style={{ width, height }}
+        className="fixed left-4 bottom-4 z-40 transition-all duration-500 cursor-pointer"
+        onClick={() => setFullscreen(invert)}
       >
-        <Image
-          width={300}
-          height={300}
-          src={image.src.medium}
-          alt={image.alt}
-          className={twMerge(
-            'absolute w-full h-full object-cover transition-opacity',
-            fullscreen && 'opacity-0',
-          )}
-        />
-        <Image
-          width={image.width}
-          height={image.height}
-          src={image.src.large2x}
-          alt={image.alt}
-          className={twMerge(
-            'absolute w-full h-full object-contain transition-opacity',
-            !fullscreen && 'opacity-0',
-          )}
-        />
+        <div className="relative rounded-lg overflow-hidden w-full h-full">
+          <Image
+            width={300}
+            height={300}
+            src={image.src.medium}
+            alt={image.alt}
+            className={twMerge(
+              'absolute w-full h-full object-contain transition-opacity',
+              fullscreen && 'opacity-0',
+            )}
+          />
+          <Image
+            width={image.width}
+            height={image.height}
+            src={image.src.large2x}
+            alt={image.alt}
+            className={twMerge(
+              'absolute w-full h-full object-contain transition-opacity',
+              !fullscreen && 'opacity-0',
+            )}
+          />
+        </div>
       </div>
-    </aside>
+    </>
   );
 }
