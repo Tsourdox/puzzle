0d20d7ee1a0e9852ddf09437893c8085404e3805
David Jensen <tsourdox@icloud.com>
2025-12-21 12:57:32 +0100

    disable strict mode (causing hard to debug issues with IndexedDB)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   images: {
     remotePatterns: [{ hostname: 'images.pexels.com' }],
   },
