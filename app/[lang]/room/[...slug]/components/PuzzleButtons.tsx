f7c8d6656f351cc2fc6d751174f387855774649e
David Jensen <tsourdox@icloud.com>
2025-12-23 19:20:55 +0100

    un-hide puzzle action buttons

--- app/[lang]/room/[...slug]/components/PuzzleButtons.tsx
+++ app/[lang]/room/[...slug]/components/PuzzleButtons.tsx
@@ -1,76 +1,74 @@
 'use client';
 
-import Button from '@/components/Button';
-import { useAtomValue } from 'jotai';
 import { showPuzzlePieceActionsAtom } from '@/app/atoms';
-import { ArrowPathIcon, RectangleGroupIcon, Square3Stack3DIcon } from '@heroicons/react/20/solid';
+import Button from '@/components/Button';
+import {
+  ArrowPathIcon,
+  RectangleGroupIcon,
+  Square3Stack3DIcon,
+  XMarkIcon,
+} from '@heroicons/react/20/solid';
 import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
-import { twMerge } from 'tailwind-merge';
+import { useAtomValue } from 'jotai';
 
-type Props = {
-  isHidden?: boolean;
-};
+interface Props {
+  isHidden: boolean;
+}
 
 export default function PuzzleButtons({ isHidden }: Props) {
   const showPuzzlePieceActions = useAtomValue(showPuzzlePieceActionsAtom);
 
-  const showPieceActionButton = !isHidden && showPuzzlePieceActions;
-
-  return null;
+  if (isHidden) return null;
 
   return (
     <>
       <Button
-        className={twMerge(
-          'transition-opacity p-2 opacity-100',
-          isHidden && 'opacity-0 pointer-events-none',
-        )}
+        className="transition-opacity p-2 opacity-100"
+        aria-label="Zoom in"
         variant="secondary"
         icon={<PlusCircleIcon width={30} height={30} />}
       />
       <Button
-        className={twMerge(
-          'transition-opacity p-2 opacity-100',
-          isHidden && 'opacity-0 pointer-events-none',
-        )}
+        className="transition-opacity p-2 opacity-100"
+        aria-label="Zoom out"
         variant="secondary"
         icon={<MinusCircleIcon width={30} height={30} />}
       />
 
-      <>
-        <Button
-          className={twMerge(
-            'transition-opacity p-2 opacity-100',
-            !showPieceActionButton && 'opacity-0 pointer-events-none',
-          )}
-          variant="secondary"
-          icon={<ArrowPathIcon width={30} height={30} />}
-        />
-        <Button
-          className={twMerge(
-            'transition-opacity p-2 opacity-100 -scale-x-100 active:-scale-x-95',
-            !showPieceActionButton && 'opacity-0 pointer-events-none',
-          )}
-          variant="secondary"
-          icon={<ArrowPathIcon width={30} height={30} />}
-        />
-        <Button
-          className={twMerge(
-            'transition-opacity p-[10px] opacity-100',
-            !showPieceActionButton && 'opacity-0 pointer-events-none',
-          )}
-          variant="secondary"
-          icon={<Square3Stack3DIcon width={26} height={26} />}
-        />
-        <Button
-          className={twMerge(
-            'transition-opacity p-[10px] opacity-100',
-            !showPieceActionButton && 'opacity-0 pointer-events-none',
-          )}
-          variant="secondary"
-          icon={<RectangleGroupIcon width={26} height={26} />}
-        />
-      </>
+      {showPuzzlePieceActions && (
+        <>
+          <Button
+            className="transition-opacity p-2 opacity-100"
+            aria-label="Rotate selected pieces counter clockwise"
+            variant="secondary"
+            icon={<ArrowPathIcon width={30} height={30} />}
+          />
+          <Button
+            className="transition-opacity p-2 opacity-100 -scale-x-100 active:-scale-x-95"
+            aria-label="Rotate selected pieces clockwise"
+            variant="secondary"
+            icon={<ArrowPathIcon width={30} height={30} />}
+          />
+          <Button
+            className="transition-opacity p-2.5 opacity-100"
+            aria-label="Stack all selected pieces"
+            variant="secondary"
+            icon={<Square3Stack3DIcon width={26} height={26} />}
+          />
+          <Button
+            className="transition-opacity p-2.5 opacity-100"
+            aria-label="Explode all selected pieces"
+            variant="secondary"
+            icon={<RectangleGroupIcon width={26} height={26} />}
+          />
+          <Button
+            className="transition-opacity p-2.5 opacity-100"
+            aria-label="Deselect pieces"
+            variant="secondary"
+            icon={<XMarkIcon width={26} height={26} />}
+          />
+        </>
+      )}
     </>
   );
 }
