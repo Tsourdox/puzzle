af5e5bdfa57a8a825f150f2af0cc33ffa718dbcb
David Jensen <tsourdox@icloud.com>
2025-12-25 14:31:53 +0100

    thicker outline selections

--- puzzle/piece.ts
+++ puzzle/piece.ts
@@ -166,15 +166,15 @@ export default class Piece implements ISerializablePiece {
 
     if (this.isSelected) {
       this.graphics.stroke('#FFF');
-      this.graphics.strokeWeight(this.size.mag() / 100);
+      this.graphics.strokeWeight(this.size.mag() / 70);
       this.drawSelectionOutline();
     } else if (this._isSelectedByOther) {
       this.graphics.stroke('#FF0');
-      this.graphics.strokeWeight(this.size.mag() / 120);
+      this.graphics.strokeWeight(this.size.mag() / 80);
       this.drawSelectionOutline();
     } else {
       this.graphics.stroke('#000');
-      this.graphics.strokeWeight(this.size.mag() / 140);
+      this.graphics.strokeWeight(this.size.mag() / 100);
       this.drawSelectionOutline();
     }
     this.graphics.pop();
